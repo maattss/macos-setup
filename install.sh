880b41cee926d03c9353cf168fdb45b4a0d5d273
Mats Tyldum <ma.tyldum@gmail.com>
2023-06-26 11:39:40 +0200

    Added mos and font smoothing adjuster

--- install.sh
+++ install.sh
@@ -61,6 +61,8 @@ brew_install iina --cask
 brew_install 1password --cask
 brew_install cheatsheet --cask
 brew_install raycast --cask
+brew_install font-smoothing-adjuster --cask
+brew_install mos --cask 
 # brew_install windscribe --cask
 
 # Check for Oh My Zsh, install if we don't have it
